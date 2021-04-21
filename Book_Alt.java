e56a5b11627b6f60eca6b2b5a5ebe6c70548920a
Seth Zacharias <75303106+Z-Target@users.noreply.github.com>
2021-04-21 16:26:05 -0600

    Create Book_Alt.java

---
+++ Book_Alt.java
@@ -0,0 +1,47 @@
+//This is an alernate version, meant to open in a separate console window
+
+
+public class Book {
+    public boolean complete;
+    public int numPagesRead;
+    public int maxPages;
+    public String bookName;
+
+    public Book(String bookName, int numPagesRead, int maxPages) {
+        this.complete = false;
+        this.bookName = bookName;
+        this.numPagesRead = numPagesRead;
+        this.maxPages = maxPages;
+    }
+
+    public Book(String bookName, int maxPages) {
+        this.complete = false;
+        this.bookName = bookName;
+        this.maxPages = maxPages;
+        this.numPagesRead = 0;
+    }
+
+    public boolean setPages(int pageNumber) {
+        // Invalid page number
+        if (pageNumber < 0 || pageNumber > maxPages) {
+            return false;
+        }
+        if (pageNumber == maxPages) {
+            this.complete = true;
+        } else {
+            this.complete = false;
+        }
+        this.numPagesRead = pageNumber;
+        return true;
+    }
+
+    public void setComplete() {
+        this.complete = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: \"" + this.bookName + "\" Completion: " + this.complete + " Pages Read: " +
+                this.numPagesRead + "/" + this.maxPages;
+    }
+}
